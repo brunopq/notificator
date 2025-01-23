7ffa5332a8cc6b82e4f9c93898361277673389fe
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2025-01-23 10:17:54 -0300

    remove host from logging middleware

--- src/common/middleware/requestLogger.ts
+++ src/common/middleware/requestLogger.ts
@@ -4,7 +4,7 @@ import type { RequestHandler } from "express"
 export const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
   const time = new Date(Date.now()).toString()
   console.log(
-    `[${format(time, "yyyy-MM-dd HH:mm:ss O")}] ${req.method} ${req.host} ${req.url}`,
+    `[${format(time, "yyyy-MM-dd HH:mm:ss O")}] ${req.method} ${req.url}`,
   )
 
   next()
