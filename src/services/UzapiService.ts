c9a996448e4fe337c30e54224cbb0279653243f6
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2025-05-23 15:45:59 -0300

    fix more stuff about uzapi

--- src/services/UzapiService.ts
+++ src/services/UzapiService.ts
@@ -6,7 +6,9 @@ import { env } from "@/common/utils/envConfig"
 import type { IWhatsappService, WhatsappResponse } from "./IWhatsappService"
 
 const UzApiVerifyNumberResponseSchema = z.object({
-  numberExists: z.boolean(),
+  profile: z.object({
+    numberExists: z.boolean(),
+  }),
 })
 
 export class UzApiService implements IWhatsappService {
@@ -27,25 +29,37 @@ export class UzApiService implements IWhatsappService {
 
   async isOnWhatsapp(phoneNumber: string) {
     console.log(`Searching for number${phoneNumber}`)
-    const res = await this.httpClient.post("/verifyNumber", {
-      session: env.WHATSAPP_INSTANCE_ID,
-      number: this.formatNumber(phoneNumber),
-    })
+    try {
+      const res = await this.httpClient.post("/verifyNumber", {
+        session: env.WHATSAPP_INSTANCE_ID,
+        number: this.formatNumber(phoneNumber),
+      })
 
-    const parsed = UzApiVerifyNumberResponseSchema.safeParse(res.data)
+      const parsed = UzApiVerifyNumberResponseSchema.safeParse(res.data)
 
-    if (!parsed.success) {
-      console.log("Could not parse response verify phone response")
-      return { exists: false as false }
-    }
+      if (!parsed.success) {
+        console.log("Could not parse response verify phone response")
+        return { exists: false as false }
+      }
 
-    if (!parsed.data.numberExists) {
-      console.log(`Number ${phoneNumber} does not appear to be on whatsapp`)
-      return { exists: false as false }
-    }
+      if (!parsed.data.profile.numberExists) {
+        console.log(`Number ${phoneNumber} does not appear to be on whatsapp`)
+        return { exists: false as false }
+      }
 
-    return {
-      exists: true as true,
+      return {
+        exists: true as true,
+      }
+    } catch (e) {
+      console.log("error while validating number")
+      if (isAxiosError(e)) {
+        if (e.response?.status === 400) {
+          console.log("not on whatsapp")
+          return { exists: false as false }
+        }
+      }
+
+      throw new Error("This will be unknown error")
     }
   }
 
